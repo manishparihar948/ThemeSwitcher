4582554a5aaba7218f068371f9a995790e736b4f
manishparihar948 <manish.parihar948@gmail.com>
2025-12-08 20:28:49 +0530

    Initial commit: setup Xcode project

---
+++ ThemeSwitcher/ThemeSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThemeSwitcher
+//
+//  Created by Manish Parihar on 08.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThemeSwitcher/ThemeSwitcher/ThemeSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSwitcherApp.swift
+//  ThemeSwitcher
+//
+//  Created by Manish Parihar on 08.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
